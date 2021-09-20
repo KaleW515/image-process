71bab60a1c7c341b7a140340eb85899a01a192b9
kalew515 <ilovewk51@gmail.com>
2021-09-20 12:51:24 +0800

    update

--- imageWork1/filter.py
+++ imageWork1/filter.py
@@ -5,172 +5,172 @@ from matplotlib import pyplot as plt
 import math
 
 
-# 添加椒盐噪音
-# prob是阈值
-def sp_noise(img, prob):
-    new_img = numpy.zeros(img.shape, numpy.uint8)
-    thres = 1 - prob
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            num = random.random()
-            if num < prob:
-                new_img[i][j] = 0
-            elif num > thres:
-                new_img[i][j] = 255
-            else:
-                new_img[i][j] = img[i][j]
-    return new_img
+class Filter:
+    __rgb = ""
+    __img = ""
 
+    def set_rgb(self, _rgb: numpy.ndarray):
+        self.__rgb = _rgb
 
-# 对每个通道进行椒盐噪声的添加
-def integration_sp_noise(img, prob):
-    b, g, r = cv2.split(img)
-    new_b = sp_noise(b, prob)
-    new_g = sp_noise(g, prob)
-    new_r = sp_noise(r, prob)
-    new_img = cv2.merge([new_b, new_g, new_r])
-    return new_img
+    def set_img(self, _img: numpy.ndarray):
+        self.__img = _img
 
+    # 添加椒盐噪音
+    # prob是阈值
+    @staticmethod
+    def __sp_noise(prob, rgb):
+        sp_rgb = numpy.zeros(rgb.shape, numpy.uint8)
+        threshold = 1 - prob
+        for i in range(rgb.shape[0]):
+            for j in range(rgb.shape[1]):
+                tmp = random.random()
+                if tmp < prob:
+                    sp_rgb[i][j] = 0
+                elif tmp > threshold:
+                    sp_rgb[i][j] = 255
+                else:
+                    sp_rgb[i][j] = rgb[i][j]
+        return sp_rgb
 
-# 添加高斯噪声
-# mean是均值, variance是方差
-def gasuss_noise(img, mean, variance):
-    img = numpy.array(img/255, dtype=float)
-    noise = numpy.random.normal(mean, variance ** 0.5, img.shape)
-    new_img = img + noise
-    if new_img.min() < 0:
-        low_clip = -1
-    else:
-        low_clip = 0
-    new_img = numpy.clip(new_img, low_clip, 1.0)
-    new_img = numpy.uint8(new_img*255)
-    return new_img
+    # 对每个通道进行椒盐噪声的添加
+    def integration_sp_noise(self, prob):
+        b, g, r = cv2.split(self.__img)
+        res_img = cv2.merge([self.__sp_noise(prob, b), self.__sp_noise(prob, g), self.__sp_noise(prob, r)])
+        return res_img
 
+    # 添加高斯噪声
+    # mean是均值, variance是方差
+    @staticmethod
+    def __gasuss_noise(rgb, mean, variance):
+        rgb = numpy.array(rgb / 255, dtype=float)
+        noise = numpy.random.normal(mean, variance ** 0.5, rgb.shape)
+        gasuss_img = rgb + noise
+        if gasuss_img.min() < 0:
+            low_clip = -1
+        else:
+            low_clip = 0
+        gasuss_img = numpy.clip(gasuss_img, low_clip, 1.0)
+        gasuss_img = numpy.uint8(gasuss_img * 255)
+        return gasuss_img
 
-# 对每个通道进行高斯噪声的添加
-def integration_gasuss_noise(img, mean, variance):
-    b, g, r = cv2.split(img)
-    new_b = gasuss_noise(b, mean, variance)
-    new_g = gasuss_noise(g, mean, variance)
-    new_r = gasuss_noise(r, mean, variance)
-    new_img = cv2.merge([new_b, new_g, new_r])
-    return new_img
+    # 对每个通道进行高斯噪声的添加
+    def integration_gasuss_noise(self, mean, variance):
+        b, g, r = cv2.split(self.__img)
+        res_img = cv2.merge([self.__gasuss_noise(b, mean, variance), self.__gasuss_noise(g, mean, variance),
+                             self.__gasuss_noise(r, mean, variance)])
+        return res_img
 
+    # 均值滤波, rgb为图像通道, value为矩阵长
+    @staticmethod
+    def __average_filtering(rgb, value):
+        border_num = int((value - 1) / 2)
+        end_rgb = numpy.zeros(rgb.shape, numpy.uint8)
+        wide = rgb.shape[0]
+        height = rgb.shape[1]
+        for i in range(rgb.shape[0]):
+            for j in range(rgb.shape[1]):
+                count = 0  # 记录周围可用块数量
+                area_sum = 0
+                for m in range(-border_num, border_num + 1):
+                    for n in range(-border_num, border_num + 1):
+                        if ((i + m) >= 0) & ((j + n) >= 0) & ((i + m) <= wide - 1) & ((j + n) <= height - 1):
+                            count += 1
+                            area_sum += rgb[i + m][j + n]
+                end_rgb[i][j] = int(area_sum / count)
+        return end_rgb
 
-# 均值滤波, rgb为图像通道, value为矩阵长
-def average_filtering(rgb, value):
-    border_num = int((value-1)/2)
-    end_rgb = numpy.zeros(rgb.shape, numpy.uint8)
-    wide = rgb.shape[0]
-    height = rgb.shape[1]
-    for i in range(rgb.shape[0]):
-        for j in range(rgb.shape[1]):
-            count = 0  # 记录周围可用块数量
-            area_sum = 0
-            for m in range(-border_num, border_num+1):
-                for n in range(-border_num, border_num+1):
-                    if ((i+m) >= 0) & ((j+n) >= 0) & ((i+m) <= wide-1) & ((j+n) <= height-1):
-                        count += 1
-                        area_sum += rgb[i+m][j+n]
-            end_rgb[i][j] = int(area_sum/count)
-    return end_rgb
+    # 为每个通道进行均值滤波处理
+    def integration_average_filtering(self, value):
+        b, g, r = cv2.split(self.__img)
+        return self.__average_filtering(b, value), self.__average_filtering(g, value), self.__average_filtering(r,
+                                                                                                                value)
 
+    # 中值滤波, rgb为图像通道, value为矩阵长
+    @staticmethod
+    def __medium_filtering(rgb, value):
+        border_num = int((value - 1) / 2)
+        end_rgb = numpy.zeros(rgb.shape, numpy.uint8)
+        wide = rgb.shape[0]
+        height = rgb.shape[1]
+        for i in range(rgb.shape[0]):
+            for j in range(rgb.shape[1]):
+                area_sum = []
+                for m in range(-border_num, border_num + 1):
+                    for n in range(-border_num, border_num + 1):
+                        if ((i + m) >= 0) & ((j + n) >= 0) & ((i + m) <= wide - 1) & ((j + n) <= height - 1):
+                            area_sum.append(rgb[i + m][j + n])
+                area_sum = sorted(area_sum)
+                end_rgb[i][j] = area_sum[len(area_sum) // 2]
+        return end_rgb
 
-# 中值滤波, rgb为图像通道, value为矩阵长
-def median_filtering(rgb, value):
-    border_num = int((value-1)/2)
-    end_rgb = numpy.zeros(rgb.shape, numpy.uint8)
-    wide = rgb.shape[0]
-    height = rgb.shape[1]
-    for i in range(rgb.shape[0]):
-        for j in range(rgb.shape[1]):
-            area_sum = []
-            for m in range(-border_num, border_num+1):
-                for n in range(-border_num, border_num+1):
-                    if ((i+m) >= 0) & ((j+n) >= 0) & ((i+m) <= wide-1) & ((j+n) <= height-1):
-                        area_sum.append(rgb[i+m][j+n])
-            area_sum = sorted(area_sum)
-            end_rgb[i][j] = area_sum[len(area_sum)//2]
-    return end_rgb
+    # 为每个通道进行中值滤波处理
+    def integration_median_filtering(self, value):
+        b, g, r = cv2.split(self.__img)
+        return self.__medium_filtering(b, value), self.__medium_filtering(g, value), self.__medium_filtering(r,
+                                                                                                             value)
 
+    # 得到高斯滤波的高斯核矩阵, value是矩阵长, sigma为方差
+    @staticmethod
+    def __get_gaussian_kernel(value, sigma):
+        border_num = int((value - 1) / 2)
+        kernel_matrix = numpy.zeros((value, value))
+        count = 0
+        for i in range(-border_num, border_num + 1):
+            for j in range(-border_num, border_num + 1):
+                kernel_matrix[i + border_num][j + border_num] = (1 / (2 * math.pi * pow(sigma, 2))) * pow(math.e, (
+                        -(i ** 2 + j ** 2) / (2 * pow(sigma, 2))))
+                count += kernel_matrix[i + border_num][j + border_num]
+        _count = 0
+        for i in range(kernel_matrix.shape[0]):
+            for j in range(kernel_matrix.shape[1]):
+                kernel_matrix[i][j] = kernel_matrix[i][j] / count
+                _count += kernel_matrix[i][j]
+        return kernel_matrix
 
-# 得到高斯滤波的高斯核矩阵, value是矩阵长, sigma为方差
-def get_gaussian_kernel(value, sigma):
-    border_num = int((value - 1)/2)
-    kernel_matrix = numpy.zeros((value, value))
-    count = 0
-    for i in range(-border_num, border_num+1):
-        for j in range(-border_num, border_num+1):
-            kernel_matrix[i+border_num][j+border_num] = (
-                1/(2*math.pi*pow(sigma, 2)))*pow(math.e, (-(i**2+j**2)/(2*pow(sigma, 2))))
-            count += kernel_matrix[i+border_num][j+border_num]
-    _count = 0
-    for i in range(kernel_matrix.shape[0]):
-        for j in range(kernel_matrix.shape[1]):
-            kernel_matrix[i][j] = kernel_matrix[i][j] / count
-            _count += kernel_matrix[i][j]
-    return kernel_matrix
+    # 根据矩阵长为原始图像的每个通道进行边缘扩充处理
+    # 处理方法是将边缘的像素向外进行扩展
+    def __get_padding_rgb(self, gaussian_kernel):
+        b, g, r = cv2.split(self.__img)
+        padding = int((gaussian_kernel.shape[0] - 1) / 2)
+        padding_b = cv2.copyMakeBorder(
+            b, padding, padding, padding, padding, cv2.BORDER_REPLICATE)
+        padding_b = padding_b.astype(numpy.uint8)
+        padding_g = cv2.copyMakeBorder(
+            g, padding, padding, padding, padding, cv2.BORDER_REPLICATE)
+        padding_g = padding_g.astype(numpy.uint8)
+        padding_r = cv2.copyMakeBorder(
+            r, padding, padding, padding, padding, cv2.BORDER_REPLICATE)
+        padding_r = padding_r.astype(numpy.uint8)
+        return padding_b, padding_g, padding_r
 
+    # 高斯滤波，　rgb为图像通道, gaussian_kernel为高斯滤波的高斯核矩阵, value为矩阵长
+    @staticmethod
+    def __gaussian_filtering(rgb, gaussian_kernel, value):
+        padding = int((gaussian_kernel.shape[0] - 1) / 2)
+        for i in range(padding, rgb.shape[0] - padding):
+            for j in range(padding, rgb.shape[1] - padding):
+                square_sum = 0
+                for m in range(-padding, padding + 1):
+                    for n in range(-padding, padding + 1):
+                        square_sum += rgb[i + m][j + n] * \
+                                      gaussian_kernel[m + padding][n + padding]
+                rgb[i][j] = square_sum
+        end_rgb = numpy.zeros((rgb.shape[0] - 2 * padding, rgb.shape[1] - 2 * padding))
+        for i in range(end_rgb.shape[0]):
+            for j in range(end_rgb.shape[1]):
+                end_rgb[i][j] = rgb[i + padding][j + padding]
+        end_rgb = end_rgb.astype(numpy.uint8)
+        return end_rgb
 
-# 根据矩阵长为原始图像的每个通道进行边缘扩充处理
-# 处理方法是将边缘的像素向外进行扩展
-def get_padding_rgb(b, g, r, gaussian_kernel):
-    padding = int((gaussian_kernel.shape[0] - 1)/2)
-    padding_b = cv2.copyMakeBorder(
-        b, padding, padding, padding, padding, cv2.BORDER_REPLICATE)
-    padding_b = padding_b.astype(numpy.uint8)
-    padding_g = cv2.copyMakeBorder(
-        g, padding, padding, padding, padding, cv2.BORDER_REPLICATE)
-    padding_g = padding_g.astype(numpy.uint8)
-    padding_r = cv2.copyMakeBorder(
-        r, padding, padding, padding, padding, cv2.BORDER_REPLICATE)
-    padding_r = padding_r.astype(numpy.uint8)
-    return padding_b, padding_g, padding_r
-
-
- # 高斯滤波，　rgb为图像通道, gaussian_kernel为高斯滤波的高斯核矩阵, value为矩阵长
-def gaussian_filtering(rgb, gaussian_kernel, value):
-    padding = int((gaussian_kernel.shape[0] - 1)/2)
-    for i in range(padding, rgb.shape[0]-padding):
-        for j in range(padding, rgb.shape[1]-padding):
-            square_sum = 0
-            for m in range(-padding, padding + 1):
-                for n in range(-padding, padding + 1):
-                    square_sum += rgb[i+m][j+n] * \
-                        gaussian_kernel[m+padding][n+padding]
-            rgb[i][j] = square_sum
-    end_rgb = numpy.zeros((rgb.shape[0]-2*padding, rgb.shape[1]-2*padding))
-    for i in range(end_rgb.shape[0]):
-        for j in range(end_rgb.shape[1]):
-            end_rgb[i][j] = rgb[i+padding][j+padding]
-    end_rgb = end_rgb.astype(numpy.uint8)
-    return end_rgb
-
-
-# 为每个通道进行均值滤波处理
-def integration_average_filtering(b, g, r, value):
-    end_b_rgb = average_filtering(b, value)
-    end_g_rgb = average_filtering(g, value)
-    end_r_rgb = average_filtering(r, value)
-    return end_b_rgb, end_g_rgb, end_r_rgb
-
-
-# 为每个通道进行中值滤波处理
-def integration_median_filtering(b, g, r, value):
-    end_b_rgb = median_filtering(b, value)
-    end_g_rgb = median_filtering(g, value)
-    end_r_rgb = median_filtering(r, value)
-    return end_b_rgb, end_g_rgb, end_r_rgb
-
-
-# 为每个通道进行高斯滤波处理
-def integration_gaussian_filtering(b, g, r, value, sigma):
-    gaussian_kernel = get_gaussian_kernel(value, sigma)
-    padding_b, padding_g, padding_r = get_padding_rgb(b, g, r, gaussian_kernel)
-    end_b_rgb = gaussian_filtering(padding_b, gaussian_kernel, value)
-    end_g_rgb = gaussian_filtering(padding_g, gaussian_kernel, value)
-    end_r_rgb = gaussian_filtering(padding_r, gaussian_kernel, value)
-    return end_b_rgb, end_g_rgb, end_r_rgb
+    # 为每个通道进行高斯滤波处理
+    def integration_gaussian_filtering(self, value, sigma):
+        gaussian_kernel = self.__get_gaussian_kernel(value, sigma)
+        padding_b, padding_g, padding_r = self.__get_padding_rgb(gaussian_kernel)
+        return self.__gaussian_filtering(padding_b, gaussian_kernel, value), self.__gaussian_filtering(padding_g,
+                                                                                                       gaussian_kernel,
+                                                                                                       value), \
+               self.__gaussian_filtering(
+                   padding_r, gaussian_kernel, value)
 
 
 # 按照像素值的分布画出灰度直方图
@@ -185,22 +185,9 @@ def draw_pic(rgb):
 
 
 if __name__ == '__main__':
-    img = cv2.imread('imageProcessing/pic.png')
-    new_img = img
-    # new_img = sp_noise(img, 0.001)
-    new_img = gasuss_noise(img, 0, 0.1)
-    b, g, r = cv2.split(new_img)
-    # cv2.imshow('pic', new_img)
-    # draw_pic(b)
-    value = 3
-    sigma = 1.5
-    # end_b_rgb, end_g_rgb, end_r_rgb = integration_average_filtering(
-    #     b, g, r, value)
-    end_b_rgb, end_g_rgb, end_r_rgb = integration_median_filtering(
-        b, g, r, value)
-    # end_b_rgb, end_g_rgb, end_r_rgb = integration_gaussian_filtering(
-    #     b, g, r, value, sigma)
-    draw_pic(end_b_rgb)
-    # cv2.imshow('pic', cv2.merge([end_b_rgb, end_g_rgb, end_r_rgb]))
+    f = Filter()
+    f.set_img(cv2.imread("pic.png"))
+    img = cv2.merge([f.integration_sp_noise(0.001)])
+    cv2.imshow("pic", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- imageWork1/pic_average.py
+++ imageWork1/pic_average.py
@@ -3,75 +3,61 @@ import numpy
 from matplotlib import pyplot as plt
 
 
-# 读图
-def read_pic():
-    img = cv2.imread('均衡化处理.png')
-    b, g, r = cv2.split(img)
-    return b, g, r
+class PicAverage:
+    __rgb = ""
+
+    def set_rgb(self, rgb: numpy.ndarray):
+        self.__rgb = rgb
+
+    # 得到通道的像素的分布字典
+    def get_rgb_dic(self):
+        rgb_dic = {}
+        rgb_max = 0
+        rgb_min = 0
+        for i in range(self.__rgb.shape[0]):
+            for j in range(self.__rgb.shape[1]):
+                rgb_max = max(rgb_max, int(self.__rgb[i][j]))
+                if self.__rgb[i][j] not in rgb_dic.keys():
+                    rgb_dic[self.__rgb[i][j]] = 1
+                else:
+                    rgb_dic[self.__rgb[i][j]] += 1
+        for i in range(rgb_max):
+            if i not in rgb_dic.keys():
+                rgb_dic[i] = 0
+        return rgb_dic, rgb_max, rgb_min
+
+    # 根据像素分布频数字典得到像素的频率分布字典
+    def get_histogram(self, rgb_dic):
+        for key in rgb_dic.keys():
+            rgb_dic[key] = rgb_dic[key] / (self.__rgb.shape[0] * self.__rgb.shape[1])
+        rgb_dic = sorted(rgb_dic.items(), key=lambda x: x[0], reverse=False)
+        return rgb_dic
+
+    # 为图像通道进行均衡化处理
+    def equalization(self):
+        rgb_dic, rgb_max, rgb_min = self.get_rgb_dic()
+        rgb_histogram_dic = dict(self.get_histogram(rgb_dic))
+        count = 0
+        for key in rgb_histogram_dic.keys():
+            count += rgb_histogram_dic[key]
+            rgb_histogram_dic[key] = count
+        diff = rgb_max - rgb_min
+        rgb_g = []
+        for key in rgb_histogram_dic.keys():
+            rgb_g.append(int(diff * rgb_histogram_dic[key] + rgb_min + 0.5))
+        return rgb_g
+
+    # 得到新的图像的直方图
+    def get_new_histogram(self, rgb_g):
+        new_matrix = numpy.zeros((self.__rgb.shape[0], self.__rgb.shape[1]))
+        for i in range(self.__rgb.shape[0]):
+            for j in range(self.__rgb.shape[1]):
+                new_matrix[i][j] = rgb_g[self.__rgb[i][j] - 1]
+        new_matrix = new_matrix.astype(numpy.uint8)
+        return new_matrix
 
 
-# 得到通道的像素的分布字典
-# rgb为图像通道
-def get_rgb_dic(rgb):
-    rgb_dic = {}
-    data = []
-    count = rgb.shape[0] * rgb.shape[1]
-    for i in range(rgb.shape[0]):
-        for j in range(rgb.shape[1]):
-            data.append(int(rgb[i][j]))
-            if rgb[i][j] not in rgb_dic.keys():
-                rgb_dic[rgb[i][j]] = 1
-            else:
-                rgb_dic[rgb[i][j]] += 1
-    g_max = max(data)
-    g_min = 0
-    for i in range(g_max):
-        if i not in rgb_dic.keys():
-            rgb_dic[i] = 0
-    return rgb_dic, count, g_max, g_min
-
-
-# 根据像素分布频数字典得到像素的频率分布字典
-# count为像素值个数总和
-def get_histogram(rgb_dic, count):
-    for key in rgb_dic.keys():
-        rgb_dic[key] = rgb_dic[key] / count
-    rgb_dic = sorted(rgb_dic.items(), key=lambda x: x[0], reverse=False)
-    return rgb_dic
-
-
-# 得到累积分布函数
-def get_cumulative_distribution(rgb_dic):
-    count = 0
-    for key in rgb_dic.keys():
-        count += rgb_dic[key]
-        rgb_dic[key] = count
-    return rgb_dic
-
-
-# 为图像通道进行均衡化处理
-def equalization(rgb):
-    b_dic, b_count, g_max, g_min = get_rgb_dic(rgb)
-    b_histogram_dic = dict(get_histogram(b_dic, b_count))
-    cumulative_distribution_dic = get_cumulative_distribution(b_histogram_dic)
-    diff = g_max - g_min
-    rgb_g = []
-    for key in cumulative_distribution_dic.keys():
-        rgb_g.append(int(diff*cumulative_distribution_dic[key] + g_min + 0.5))
-    return rgb_g
-
-
-# 得到新的图像的直方图
-def get_new_histogram(rgb_g, rgb):
-    new_matrix = numpy.zeros((rgb.shape[0], rgb.shape[1]))
-    for i in range(rgb.shape[0]):
-        for j in range(rgb.shape[1]):
-            new_matrix[i][j] = rgb_g[rgb[i][j]-1]
-    new_matrix = new_matrix.astype(numpy.uint8)
-    return new_matrix
-
-
-#根据像素分布画出直方图
+# 根据像素分布画出直方图
 def draw_pic(rgb):
     data = []
     for i in range(rgb.shape[0]):
@@ -83,13 +69,14 @@ def draw_pic(rgb):
 
 
 if __name__ == '__main__':
-    b, g, r = read_pic()
-    b_g = equalization(b)
-    g_g = equalization(g)
-    r_g = equalization(r)
-    new_b_matrix = get_new_histogram(b_g, b)
-    new_g_matrix = get_new_histogram(g_g, g)
-    new_r_matrix = get_new_histogram(r_g, r)
+    b, g, r = cv2.split(cv2.imread("248978-106.jpg"))
+    pic = PicAverage()
+    pic.set_rgb(b)
+    new_b_matrix = pic.get_new_histogram(pic.equalization())
+    pic.set_rgb(g)
+    new_g_matrix = pic.get_new_histogram(pic.equalization())
+    pic.set_rgb(r)
+    new_r_matrix = pic.get_new_histogram(pic.equalization())
     # draw_pic(new_b_matrix)
     cv2.imshow('pic', cv2.merge([new_b_matrix, new_g_matrix, new_r_matrix]))
     cv2.waitKey(0)
